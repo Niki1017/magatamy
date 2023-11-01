8f85dcd0b369f93f95030dc07a5680cb2c8f6e66
Niki1017 <108806120+Niki1017@users.noreply.github.com>
2023-11-01 22:02:11 +0500

    Add files via upload

--- js/discordpartners.js
+++ js/discordpartners.js
@@ -1,21 +1,23 @@
+let langData = [["Язык", "Мова", "Language"], ["Социальная сеть", "соціальні мережі", "social network"]]
+
 let card = document.getElementById("cardImageUser")
 
-var users = {"493318176189448204": {}, "961256514427969607": {}, "513975760189390855": {}, "463661523181502464": {}, "227117061011144704": {}};
+var users = { "493318176189448204": {userDesc: "API разработчик"}, "961256514427969607": {userDesc: "frondEnd разработчик"}, "513975760189390855": {userDesc: "Основатель проекта"}, "463661523181502464": {userDesc: "Блогер"}, "227117061011144704": {userDesc: "Правая рука"} };
 
 function getUserCard(userId) {
-    return fetch(`https://magatamy.com/api/v1/discord/users/${userId}`)
-        .then(response => response.json())
-        .then(commits => {
-            console.log(commits);
-            let avatar = (commits.avatar);
-            let global_name = (commits.global_name);
-            let cardUser = `
+  return fetch(`https://magatamy.com/api/v1/discord/users/${userId}`)
+    .then(response => response.json())
+    .then(commits => {
+      console.log(commits);
+      let avatar = (commits.avatar);
+      let global_name = (commits.global_name);
+      let cardUser = `
     <div class="cardCommand">
     <img
       src="https://cdn.discordapp.com/avatars/${userId}/${avatar}.webp?size=128"
       alt="Person" class="card__image">
     <p class="card__name">${global_name}</p> <br>
-    <p class="card__p">Блогер</p>
+    <p class="card__p">${users[userId].userDesc}</p>
     <ul class="social-icons">
       <li><a href="https://www.youtube.com/channel/UCdrZrlodik1odF5hrHr5w3Q" target="_blank"><ion-icon
             name="logo-youtube"></ion-icon></a></li>
@@ -23,9 +25,9 @@ function getUserCard(userId) {
     </ul>
   </div>
 `
-card.innerHTML=card.innerHTML+cardUser
+      card.innerHTML = card.innerHTML + cardUser
 
-})
+    })
 }
 for (var userId in users) {
   getUserCard(userId);
